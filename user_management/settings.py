fbca710db38c423697da72617da3c52b2cb21a4a
Stas Zosimov <zosimovv@yandex.ru>
2025-11-08 00:40:17 +0300

    Update settings.py

--- user_management/settings.py
+++ user_management/settings.py
@@ -90,18 +90,12 @@ WSGI_APPLICATION = 'user_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 
 
 
-"""
+
 
 DATABASES = {
      'default': {
@@ -113,6 +107,18 @@ DATABASES = {
          'PORT': '5432',
      }
  }
+
+"""
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
 
 
 
